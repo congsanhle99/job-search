c5660034e027b5d3e9265279a954f58b4e0f98c7
CongSanhLe <congsanhle1999@gmail.com>
2023-08-24 23:00:19 +0700

    [0408] get topic stats

--- backend/job/views.py
+++ backend/job/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.db.models import Avg, Min, Max, Count
 from .serializers import JobSerializer
 from .models import Job
 
@@ -61,3 +61,24 @@ def deleteJob(request, pk):
     job.delete()
 
     return Response({'message': 'Job is deleted!!!'}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def getTopicStats(request, topic):
+    args = {'title__icontains': topic}
+    print("args: ", args)
+    jobs = Job.objects.filter(**args)
+    print("jobs: ", jobs)
+
+    if len(jobs) == 0:
+        return Response({'message': 'Not stats found for {topic}'.format(topic=topic)})
+
+    stats = jobs.aggregate(
+        total_jobs=Count('title'),
+        avg_positions=Avg('positions'),
+        avg_salary=Avg('salary'),
+        min_salary=Min('salary'),
+        max_salary=Max('salary'),
+    )
+
+    return Response(stats)
