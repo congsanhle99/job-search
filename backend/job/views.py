49c572620628a9259c51535d4fc8abfafbe77c45
CongSanhLe <congsanhle1999@gmail.com>
2023-08-24 22:08:15 +0700

    [0403] get all jobs n serializing data

--- backend/job/views.py
+++ backend/job/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import JobSerializer
+from .models import Job
 
 # Create your views here.
+# define endpoint
+
+
+@api_view(['GET'])
+# get all jobs
+def getAllJobs(request):
+    jobs = Job.objects.all()
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
