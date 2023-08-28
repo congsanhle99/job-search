2f7fec0f47dda25c985e569e482c223b7529f931
CongSanhLe <congsanhle1999@gmail.com>
2023-08-28 18:31:22 +0700

    [0608] update user profile

--- backend/account/views.py
+++ backend/account/views.py
@@ -43,3 +43,26 @@ def register(request):
 def currentUser(request):
     user = UserSerializer(request.user)
     return Response(user.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request):
+    user = request.user
+    print("updateUser USER: ", user)
+
+    data = request.data
+
+    user.first_name = data['first_name']
+    user.last_name = data['last_name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+
+    serializer = UserSerializer(user, many=False)
+
+    return Response(serializer.data)
