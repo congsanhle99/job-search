32189f3bdb2d61b68776f3b4c36e8dbdb7d0990e
CongSanhLe <congsanhle1999@gmail.com>
2023-08-29 16:29:06 +0700

    [0901] modeling candidates applied

--- backend/job/models.py
+++ backend/job/models.py
@@ -81,3 +81,10 @@ class Job(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class CandidatesApplied(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    resume = models.CharField(max_length=200)
+    appliedAt = models.DateTimeField(auto_now_add=True)
