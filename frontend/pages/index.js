d7f4081cdc332112acef6ef4a37c7ac1606f373b
CongSanhLe <congsanhle1999@gmail.com>
2023-09-05 16:47:16 +0700

    [1202] adding pagination

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,8 +13,9 @@ export default function Index({ data }) {
 export async function getServerSideProps({ query }) {
   const keyword = query.keyword || "";
   // const location = query.location || "";
-  const queryStr = `keyword=${keyword}`;
-  // const queryStr = `keyword=${keyword}&location=${location}`
+  const page = query.page || "";
+  const queryStr = `keyword=${keyword}&page=${page}`;
+  // const queryStr = `keyword=${keyword}&location=${location}&page=${page}`
 
   const res = await axios.get(`${process.env.API_URL}/api/jobs?${queryStr}`);
   const data = res.data;
