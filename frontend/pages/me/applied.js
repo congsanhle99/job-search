ac10eb49367f9269460afaa057bbf71a0c36be1c
CongSanhLe <congsanhle1999@gmail.com>
2023-09-12 15:47:34 +0700

    [1506] get applied jobs component

--- frontend/pages/me/applied.js
+++ frontend/pages/me/applied.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import JobsApplied from "../../components/job/JobsApplied";
 import Layout from "../../components/layout/Layout";
 import { isAuthenticatedUser } from "../../utils/isAuthenticated";
 
 export default function JobsAppliedPage({ jobs }) {
   return (
     <Layout title="Jobs Applied">
-      <h1>Jobs Applied</h1>
+      <JobsApplied jobs={jobs} />
     </Layout>
   );
 }
